56ee80436c8c0d999b759670bb2b278266efcf9d
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-11-09 18:33:46 +0600

    67-4 Type Definitions & Route Configuration in Proxy File

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,36 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+
+type UserRole = "ADMIN" | "DOCTOR" | "PATIENT";
+
+// exact : ["/my-profile", "settings"]
+// patterns  : [/^\/dashboard/, /^\/patient/] // routes starting with /dashboard/* and /admin/*
+type RouteConfig ={
+  exact : string[],
+  patterns : RegExp[]
+}
+
+const authRoutes = ["/login", "/register", "/forgot-password", "/reset-password"];
+
+const commonProtectedRoutes : RouteConfig = {
+  exact : ["/my-profile", "/settings"],
+  patterns :[] // [password/change-password, /password/reset-password => /password/*]
+}
+
+const doctorProtectedRoutes : RouteConfig = {
+  patterns : [/^\/doctor/], //routes starting with /doctor/*
+  exact : [] // /assistants
+}
+
+const adminProtectedRoutes : RouteConfig = {
+  patterns : [/^\/admin/], // routes starting with /admin/*
+  exact : []
+}
+
+const patientProtectedRoutes : RouteConfig = {
+  patterns : [/^\/dashboard/], // routes starting with /dashboard/*
+  exact : []
+}
  
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
