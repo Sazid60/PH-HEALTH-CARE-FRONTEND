8c7a45e1c0c83f86249cc4498a5a94b4bb499da7
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-11-12 20:46:29 +0600

    68-8 Making DashboardSidebar Component

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,8 +1,21 @@
+import { getUserInfo } from "@/services/auth/getUserInfo";
+import { UserInfo } from "@/types/user.interface";
+import DashboardSidebarContent from "./DashboardSidebarContent";
+import { getDefaultDashboardRoute } from "@/lib/auth-utils";
+import { NavSection } from "@/types/dashboard.interface";
 
-const DashboardSidebar = () => {
+const DashboardSidebar = async () => {
+    const userInfo = (await getUserInfo()) as UserInfo;
+
+    const navItems : NavSection[] = [];
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role);
     return (
         <div>
-            <h1> Dashboard Sidebar</h1>
+            <DashboardSidebarContent 
+            userInfo={userInfo}
+            navItems={navItems}
+            dashboardHome={dashboardHome}
+            />
         </div>
     );
 };
