f66e9315a33c92a765073d3b79918f65631f779a
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-11-22 01:31:18 +0600

    70-7 Creating Reusable UserInfoCell, StatusBadgeCell, DateCell And Doctors Table, 70-8 Getting Query Object From searchParams And Formatting The Query String, 70-9 Adding Pagination Component For Doctors Table, 70-10 Creating View Details Modal To View Doctor Detail From Table,70-11 Completing Edit Functionality To Edit The Doctor From Table

--- src/components/shared/RefreshButton.tsx
+++ src/components/shared/RefreshButton.tsx
@@ -5,37 +5,40 @@ import { useTransition } from "react";
 import { Button } from "../ui/button";
 
 interface RefreshButtonProps {
-    size?: "sm" | "default" | "lg";
-    variant?: "default" | "outline" | "ghost";
-    showLabel?: boolean;
+  size?: "sm" | "default" | "lg";
+  variant?: "default" | "outline" | "ghost";
+  showLabel?: boolean;
 }
 
 const RefreshButton = ({
-    size = "default",
-    variant = "default",
-    showLabel = true,
+  size = "default",
+  variant = "default",
+  showLabel = true,
 }: RefreshButtonProps) => {
-    const router = useRouter();
-    const [isPending, startTransition] = useTransition(); // lets us re render a part of the of the ui not the full page/ full component. 
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-
-    const handleRefresh = () => {
-        startTransition(() => {
-            router.refresh(); // because of using transition it will run in background without blocking the UI
-        });
-    };
-    return (
-        <Button
-            size={size}
-            variant={variant}
-            onClick={handleRefresh}
-            disabled={isPending}
-        >
-            <RefreshCcw className={`h-4 w-4 ${isPending ? "animate-spin" : ""} ${showLabel ? "mr-2" : ""}`}
-            />
-            {showLabel && "Refresh"}
-        </Button>
-    );
+  const handleRefresh = () => {
+    startTransition(() => {
+      router.refresh();
+    });
+  };
+  return (
+    <Button
+      size={size}
+      variant={variant}
+      onClick={handleRefresh}
+      disabled={isPending}
+    >
+      <RefreshCcw
+        className={`h-4 w-4 ${isPending ? "animate-spin" : ""} ${
+          showLabel ? "mr-2" : ""
+        }`}
+      />
+      {showLabel && "Refresh"}
+    </Button>
+  );
 };
 
 export default RefreshButton;
+
